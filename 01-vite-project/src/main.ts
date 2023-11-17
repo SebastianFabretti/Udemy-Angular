7cf5e40fa6db700088403ef1c9553abe3419fa5e
Sebb <sebastianfabretti@gmail.com>
2023-11-17 17:58:09 -0300

    Topico 1 y 2

--- 01-vite-project/src/main.ts
+++ 01-vite-project/src/main.ts
@@ -1,4 +1,6 @@
 import './style.css'
+import './topics/02-object-interface'
+//import './topics/01-basic-types'
 //import typescriptLogo from './typescript.svg'
 //import viteLogo from '/vite.svg'
 //import { setupCounter } from './counter.ts'
---
+++ 01-vite-project/src/topics/01-basic-types.ts
@@ -0,0 +1,11 @@
+const name: string = 'Strider'
+let hpPoints: number | string = 90
+const isAlive: boolean = true 
+
+console.log({
+    name, hpPoints, isAlive
+})
+
+hpPoints = "full";
+
+export{};
