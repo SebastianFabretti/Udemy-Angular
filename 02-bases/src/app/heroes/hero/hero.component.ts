84e94d5fd1b2438c022cf6b1c9929a6f432aa925
Sebb <sebastianfabretti@gmail.com>
2023-12-21 18:09:56 -0300

    Topic 4

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./hero.component.css']
 })
 export class HeroComponent {
+  public name: string = 'Superman';
+  public age: number = 35;
 
+  get capitalizedName():string{
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string {
+    return `${this.name} - ${this.age}`
+  }
+
+  changeName(): void {
+    this.name = "Batman"
+  }
+
+  changeAge(): void {
+    this.age = 30
+  }
 }
