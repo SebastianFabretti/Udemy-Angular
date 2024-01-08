50ed3df7490139f0225e6eeb20436b65b255ca70
Sebb <sebastianfabretti@gmail.com>
2024-01-08 17:52:06 -0300

    Topic 5

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { CounterModule } from './counter/counter.module';
-import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     CounterModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
