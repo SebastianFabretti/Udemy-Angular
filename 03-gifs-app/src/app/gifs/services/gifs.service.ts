bf44cf3a8f75da91dace64839f819bade19bffb5
Sebb <sebastianfabretti@gmail.com>
2024-03-27 18:01:21 -0300

    Video 96

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,12 +1,17 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchResponse, Gif } from '../interfaces/gifs.interfaces';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
+  public gifList: Gif[] = []
+
   private _tagsHistory: string[] = [];
   private apiKey: string = 'R32twIfhNmrrKOyMQAEyhdfExzDUrA9P'
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get tagsHistory(){
     return [...this._tagsHistory];
@@ -25,6 +30,17 @@ export class GifsService {
 
   searchTag(tag: string): void{
     if (tag.length === 0) return;
-    this.organizeHistory(tag)
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', tag)
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+    .subscribe(resp => {
+      this.gifList = resp.data
+      console.log({gifs: this.gifList})
+    })
   }
 }
